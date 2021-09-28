1afe2cc070a4efa3346feddc9d9d0881a38013da
Nkosana11Eleven <nkosana@stsafrica.com>
2021-09-28 12:15:05 +0200

    bug

--- popup/popup.js
+++ popup/popup.js
@@ -86,10 +86,10 @@ exportButton.onclick = function(){
         //public static By saveRiskItemButton = By.xpath("//button[contains(@class,'btn btn-primary')]");
 
         if(isXpath(saved_elements[x].xpath)){
-            stringData += "//" + saved_elements[x].comment +  "\npublic static By " + saved_elements[x].variableName + " = By.xpath(" + saved_elements[x].xpath + ");\n\n"
+            stringData += "//" + saved_elements[x].comment +  "\npublic static By " + saved_elements[x].variableName + " = By.xpath(\"" + saved_elements[x].xpath + "\");\n\n"
         }
         else{
-            stringData +=  "//" + saved_elements[x].comment + "\npublic static By " + saved_elements[x].variableName + " = By.id(\"" + saved_elements[x].xpath +");\n\n"
+            stringData +=  "//" + saved_elements[x].comment + "\npublic static By " + saved_elements[x].variableName + " = By.id(\"" + saved_elements[x].xpath +"\");\n\n"
         }
 
         // "\npublic static By" + saved_elements[x].variableName + " = By.xpath(" + saved_elements[x].xpath + ");"
